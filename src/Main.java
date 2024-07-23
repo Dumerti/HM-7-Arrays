168f25f65b52694ad03659e89be88862e2f4e821
Dumer <dumeti@gmail.com>
2024-07-23 14:42:55 +0500

    HM correction of comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
@@ -6,13 +5,13 @@ public class Main {
         System.out.println();
         System.out.println("Задаче №1");
 
-        int[] weight = new int[12];
+        int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
         System.out.println(Arrays.toString(weight));
 
-        double[] weight2 = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
+        double[] weight2 = {1.57, 7.654, 9.986};
         System.out.println(Arrays.toString(weight2));
 
         int[] weight3 = new int[24];
@@ -54,28 +53,37 @@ public class Main {
         System.out.println("Задача №3");
 
         for (int i = weight.length - 1; i >= 0; i--) {
-            System.out.print(weight[i] + ", ");
+            System.out.print(weight[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
         for (int i = weight2.length - 1; i >= 0; i--) {
-            System.out.print(weight2[i] + ", ");
+            System.out.print(weight2[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
         for (int i = weight3.length - 1; i >= 0; i--) {
-            System.out.print(weight3[i] + ", ");
+            System.out.print(weight3[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+
+
         System.out.println();
         System.out.println("Задача №4");
 
-        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int i = 0; i < arr.length;  i ++) {
-            if (arr[i] % 2 != 0) {
-                arr[i] = arr[i] + 1;
-                System.out.print(arr[i] + ", ");
+        for (int i = 0; i < weight.length; i++) {
+            if (weight[i] % 2 != 0) {
+                weight[i] = weight[i] + 1;
+                System.out.print(weight[i] + ", ");
             }
-            System.out.print(arr[i] + ", ");
         }
 
 
